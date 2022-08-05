55dc23bb73eae600ec0b1d10ecf23f69a77190db
laxman-22 <69215570+laxman-22@users.noreply.github.com>
2022-08-05 11:13:21 -0400

    Add files via upload

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -14,7 +14,7 @@
             <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d370522.04242599895!2d-80.07994397523673!3d43.487658648180705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x488e130228fc96fd%3A0x2dd2cd05272e3229!2sElusive%20Auto%20Care!5e0!3m2!1sen!2sca!4v1658967334435!5m2!1sen!2sca" width="600" height="450" style="border:0; outline: 2px solid lightgray" allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
         <center>
-            <p>#KeepItElusive</p>
+            <p style="color: white; text-align: center">#KeepItElusive</p>
         </center>
     </div>
 </template>
